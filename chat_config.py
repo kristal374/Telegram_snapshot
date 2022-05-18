0d61094bb467e766cfc4b0698961c8a9a1fee3ec
kristal <Aleks.golovany@gmail.com>
2022-05-18 18:40:28 +0300

    Добавлена функция классифицирующая сообщения

--- chat_config.py
+++ chat_config.py
@@ -1,4 +1,7 @@
-from telethon import *
+import telethon
+from telethon.tl.types import DocumentAttributeAudio, DocumentAttributeImageSize, DocumentAttributeFilename, \
+    DocumentAttributeSticker, MessageMediaContact, MessageMediaWebPage, MessageActionPhoneCall, MessageMediaPhoto, \
+    MessageMediaDocument, MessageMediaGeo
 from datebase import DataBase
 from const import *
 
@@ -68,11 +71,57 @@ async def get_member(event, client):
     real_sender_name = real_sender_name if real_sender_name else await get_name(real_sender_id)
     return await get_name(sender_id), sender_id, real_sender_name, real_sender_id
 
+async def type_message(message):
+    type_mes = None
+    if isinstance(message.media, MessageMediaPhoto):
+        type_mes = 'Photo'
+    elif isinstance(message.media, MessageMediaWebPage):
+        type_mes = 'Link'
+    elif isinstance(message.action, MessageActionPhoneCall):
+        type_mes = 'PhoneCall'
+    elif isinstance(message.media, MessageMediaGeo):
+        type_mes = 'GeoPosition'
+    elif isinstance(message.media, MessageMediaContact):
+        type_mes = 'Contact'
+    elif isinstance(message.media, MessageMediaDocument) and message.video is not None:  # video
+        if message.video.attributes[0].round_message is True:
+            type_mes = "CircularVideo"
+        elif len(message.video.attributes) in (1, 2):
+            type_mes = 'Video'
+        elif len(message.video.attributes) == 3:
+            type_mes = "GIF"
+    elif message.media is not None:
+        if isinstance(message.media.document.attributes[0], DocumentAttributeAudio):
+            if len(message.media.document.attributes) == 2:
+                type_mes = "Music"
+            else:
+                type_mes = "VoiceMessage"
+        elif isinstance(message.media.document.attributes[0], DocumentAttributeImageSize):
+            target = message.media.document.attributes
+            if type(target[1]) == DocumentAttributeSticker:
+                type_mes = 'Sticker'
+            elif type(target[1]) == DocumentAttributeFilename:
+                type_mes = 'PhotoFile'
+        elif isinstance(message.media.document.attributes[0], DocumentAttributeFilename):
+            if message.media.document.mime_type == 'application/pdf':
+                type_mes = 'PDF'
+            elif message.media.document.thumbs is not None:
+                type_mes = 'VideoFile'
+            else:
+                type_mes = 'File'
+    elif message.text != 'Text':
+        type_mes = 'Text'
+    else:
+        type_mes = 'ErrorTypeFile'
+    return type_mes
+
+
 async def new_message(event, client):
     chat_id = event.chat_id
     db.transactions("SELECT active FROM chat WHERE chat_id={}".format(chat_id))
     tracking = db.cursor.fetchone()[0]
     print(await get_member(event, client))
+    print(await type_message(event.message))
     if tracking:
         print(event.message.to_dict()['message'])
     # else:
