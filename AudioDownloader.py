4cc8c471a6a2d8016db88abf8221da17d8cbe310
kristal <Aleks.golovany@gmail.com>
2022-05-20 10:02:26 +0300

    Добавлен ещё один обработчик ошибок Убрано ненужное переименование модуля

--- AudioDownloader.py
+++ AudioDownloader.py
@@ -1,18 +1,20 @@
-import speech_recognition as speech_recognizer
+import speech_recognition
 from pydub import AudioSegment
 import io
 
 
 def audio_to_text(file):
-    sample_audio = speech_recognizer.AudioFile(convertFromOgg(file))
-    recognizer = speech_recognizer.Recognizer()
+    sample_audio = speech_recognition.AudioFile(convertFromOgg(file))
+    recognizer = speech_recognition.Recognizer()
     with sample_audio as audio_file:
         recognizer.adjust_for_ambient_noise(audio_file)
         audio_content = recognizer.record(audio_file)
     try:
         text = recognizer.recognize_google(audio_content, language='ru')
-    except speech_recognizer.UnknownValueError:
-        text = 'Текст не распознан...'
+    except speech_recognition.UnknownValueError:
+        text = 'Текст не распознан'
+    except speech_recognition.RequestError:
+        text = 'Ошибка во время преобразования'
     return text
 
 
