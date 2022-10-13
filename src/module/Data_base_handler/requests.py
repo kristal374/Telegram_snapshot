129976a10b1cfc47e9f10e1653ed659cc3f292a6
kristal <Aleks.golovany@gmail.com>
2022-10-13 19:57:08 +0300

    new method for working with a table "author"

--- src/module/Data_base_handler/requests.py
+++ src/module/Data_base_handler/requests.py
@@ -1,6 +1,17 @@
 from .models import *
 
 
+def search_author(id_):
+    try:
+        return Author.select().where(Author.author_id == id_).get()
+    except Author.DoesNotExist:
+        return None
+
+
+def add_new_author(author):
+    Author.create(**author.__dict__)
+
+
 def get_all_chat():
     return Chat.select()
 
@@ -9,5 +20,5 @@ def add_new_chat(chat):
     Chat.create(**chat.__dict__)
 
 
-def add_new_message(messages):
-    Message.create(**messages.__dict__)
+def add_new_message(messages): ...
+    # Message.create(**messages.__dict__)
